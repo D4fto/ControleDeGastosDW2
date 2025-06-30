d898ad4e29cd32c76059dc08292fda7648333bb7
D4fto <163424876+D4fto@users.noreply.github.com>
2025-06-29 21:41:38 -0300

    Update Group.jsx

--- src/Group.jsx
+++ src/Group.jsx
@@ -1,7 +1,7 @@
 export default function Group({nome, descricao, valor, list}){
     return(<li className="Grupo">
         <div className="GrupoInfo">
-            <div className="NamePrice">
+            <div className="NomePreco">
                 <p><i className="bi bi-folder2"></i> {nome}</p> <p>R$ {String(valor).padStart(3,"0").substring(0,String(valor).padStart(3,"0").length-2)},{String(valor).padStart(3,"0").substring(String(valor).padStart(3,"0").length-2)}</p>
             </div>
             <div className="Descricao">{descricao}</div>
