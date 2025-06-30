44bef12cdde1d650c6d704f7fd111641cfb262fd
marceloAst <marcelo.2007@alunos.utfpr.edu.br>
2025-06-29 21:02:12 -0300

    0.2

--- src/App.jsx
+++ src/App.jsx
@@ -148,9 +148,15 @@ function App() {
     <>
     <nav className='MenuHorizontal'>
       <h1 className='Titulo'>Investec</h1>
+<<<<<<< Updated upstream
       <div>
         <button onClick={()=>setvariavel("despesas")}>Despesas</button>
         <button onClick={()=>setvariavel("dashboard")}>Dashboard</button>
+=======
+      <div>
+        <button>Despesas</button>
+        <button>Dashboard</button>
+>>>>>>> Stashed changes
       </div>
     </nav>
     <div>
