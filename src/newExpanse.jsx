47c04732289f4fc05356de4e8e6c4558c83ce22f
D4fto <163424876+D4fto@users.noreply.github.com>
2025-07-01 10:20:58 -0300

    Update newExpanse.jsx

--- src/newExpanse.jsx
+++ src/newExpanse.jsx
@@ -51,25 +51,29 @@ export default function NewExpanse({data, setData, setVariavel}){
                 Nova Despesa
             </h1>
             <form onSubmit={send} action="">
-                <label htmlFor="nome">Nome: </label>
+                <label htmlFor="nome">Nome: </label><br />
                 <input onChange={(e)=>{changeObject("nome",e.target.value)}} id="nome" type="text" placeholder="Nome" required/> <br />
-                <label htmlFor="valor">valor: </label>
+                <label htmlFor="valor">valor: </label><br />
                 <input onChange={(e)=>{changeObject("valor",e.target.value)}} id="valor" type="number" placeholder="R$ 00.00" required/> <br />
-                <label htmlFor="data">Data: </label>
-                <input onChange={(e)=>{changeObject("data",e.target.value)}} id="data" type="date" required/> <br />
-                <label htmlFor="descricao">Descrição: </label>
+                <label htmlFor="descricao">Descrição: </label><br />
                 <textarea onChange={(e)=>{changeObject("descricao",e.target.value)}} name="" id="descricao" placeholder="Descrição legal"></textarea> <br />
                 <div style={{position: "relative", width: "fit-content"}}>
                     <label htmlFor="local">Local: </label>
                     <PopUpButton id="local" title={group} PopUp={ChooseGroup} props={{data: data, setData: setData, type:type, group: group, setGroup: setGroup}}/><br/>
                 </div>
-                <label htmlFor="categoria">Categoria: </label>
-                <select onChange={(e)=>{changeObject("categoria",e.target.value)}} name="" id="categoria">
-                {data.categories.map((e, k)=>{return <option key={k} value={e}>{e}</option>})}
-                </select> <br />
-                <label><input type="radio" name="tipo" id="fixo" onChange={()=>{setType("fixo")}} checked={type === "fixo"}/> Fixo </label> <br />
-                <label><input type="radio" name="tipo" id="variavel" onChange={()=>{setType("variavel")}} checked={type === "variavel"}/> Variável </label> <br />
-                <button>submit</button>
+                <div className="DataCategoria">
+                    <label htmlFor="data">Data: </label><br />
+                    <input onChange={(e)=>{changeObject("data",e.target.value)}} id="data" type="date" required/> <br />
+                    <label htmlFor="categoria">Categoria: </label>
+                    <select onChange={(e)=>{changeObject("categoria",e.target.value)}} name="" id="categoria">
+                    {data.categories.map((e, k)=>{return <option key={k} value={e}>{e}</option>})}
+                    </select> 
+                </div>
+                <div className="TipoSubmit">
+                    <label><input type="radio" name="tipo" id="fixo" onChange={()=>{setType("fixo")}} checked={type === "fixo"}/> Fixo </label>
+                    <label><input type="radio" name="tipo" id="variavel" onChange={()=>{setType("variavel")}} checked={type === "variavel"}/> Variável </label> <br />
+                    <button>submit</button>
+                </div>
             </form>
         </div>
     );
