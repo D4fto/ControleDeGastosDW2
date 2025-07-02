3f79adf859800c1405100bf7b2bc6c47dbc44c96
D4fto <163424876+D4fto@users.noreply.github.com>
2025-07-02 13:02:25 -0300

    AddGroup
    Agora é possivel adicionar grupos também

--- src/CategoryList.jsx
+++ src/CategoryList.jsx
@@ -1,19 +1,22 @@
 export default function CategoryList({data}){
     const pin = {}
+    const values = {}
     for (const element of data.categories) {
         pin[element]=[]
+        values[element]=0
     }
     for (const key in data.expanses.fixo) {
         if (key!="maxId") {
             const element = data.expanses.fixo[key];
             pin[element.categoria].push({"nome":element.nome,"valor":element.valor})
-            
+            values[element.categoria]+=parseInt(element.valor)
         }
     }
     for (const key in data.expanses.variavel) {
         if (key!="maxId") {
             const element = data.expanses.variavel[key];
             pin[element.categoria].push({"nome":element.nome,"valor":element.valor})
+            values[element.categoria]+=parseInt(element.valor)
             
         }
     }
@@ -27,11 +30,11 @@ export default function CategoryList({data}){
                 {data.categories.map((e)=>{
                     return(
                         <div>
-                            <h4>{e}</h4>
+                            <h4>{e} - R$ {String(values[e]).padStart(3, "0").substring(0, String(values[e]).padStart(3, "0").length - 2)},{String(values[e]).padStart(3, "0").substring(String(values[e]).padStart(3, "0").length - 2)}</h4>
                             <ul>
                                 {pin[e].map((t)=>(
                                 <li>
-                                    <p>{t.nome} - {t.valor}</p>
+                                    <p>{t.nome} - R$ {String(t.valor).padStart(3, "0").substring(0, String(t.valor).padStart(3, "0").length - 2)},{String(t.valor).padStart(3, "0").substring(String(t.valor).padStart(3, "0").length - 2)}</p>
                                 </li>
                                 ))}
                             </ul>
--- src/NewGroup.jsx
+++ src/NewGroup.jsx
@@ -3,25 +3,82 @@ import PopUpButton from "./PopUpButton"
 import ChooseGroup from "./ChooseGroup"
 import "./PopUp.css"
 
+
+function findGroupToAdd(address, groups, nome, descricao) {
+    for (const element of groups) {
+      if (element.nome == address[0]) {
+        address.shift();
+        addIdToGroup(address, element, nome, descricao);
+        return;
+      }
+    }
+    console.error(address);
+    console.error(`Grupo não encontrado`);
+}
+function addIdToGroup(address, group, nome, descricao) {
+    if (!address.length) {
+        group.children.groups.push({
+            nome: nome,
+            descricao: descricao,
+            valor: 0,
+            children: {
+            groups: [],
+            expanses: [],
+            },
+        });
+      return;
+    }
+    findGroupToAdd(address, group.children.groups, nome, descricao);
+  }
+
 export default function NewGroup({data, setData, setControl}){
     const [group, setGroup] = useState("/")
+    const [nome, setNome] = useState("")
+    const [descricao, setDescricao] = useState("")
     const [type, setType] = useState("fixo")
+
+
     useEffect(()=>{
         setGroup("/")
     }, [type])
     function adicionarGrupo(e){
         e.preventDefault()
+        const newData = { ...data };
+
+        if (group === "/") {
+          newData.groups[type].push({
+            nome: nome,
+            descricao: descricao,
+            valor: 0,
+            children: {
+            groups: [],
+            expanses: [],
+            },
+        });
+        } else {
+          findGroupToAdd(
+            group.substring(1, group.length - 1).split("/"),
+            newData.groups[type],
+            nome,
+            descricao
+          );
+        }
+        setData(newData);
+        setControl(false);
+    }
+    function verifySend(){
+
     }
     return(<div className="PopUp novoGrupo">
         <button onClick={()=>{setControl(false)}}><i className="bi bi-x"></i></button>
         <h1>Novo Grupo</h1>
         <form onSubmit={adicionarGrupo}>
-            <input type="text" name="nome" id="nome" placeholder="Nome do grupo" required/><br />
-            <textarea name="descricao" id="descricao"></textarea><br />
+            <input type="text" name="nome" id="nome" placeholder="Nome do grupo" onChange={(e)=>{setNome(e.target.value)}} required/><br />
+            <textarea name="descricao" id="descricao" onChange={(e)=>{setDescricao(e.target.value)}} value={descricao}></textarea><br />
             <PopUpButton id="local" title={group} PopUp={ChooseGroup} props={{data: data, setData: setData, type:type, group: group, setGroup: setGroup}}/><br />
             <label><input type="radio" name="tipo" id="fixo" onChange={()=>{setType("fixo")}} checked={type === "fixo"}/> Fixo </label>
             <label><input type="radio" name="tipo" id="variavel" onChange={()=>{setType("variavel")}} checked={type === "variavel"}/> Variável </label> <br />
-            <button>+</button>
+            <button disabled={nome===""} style={{cursor: nome===""?"unset":""}}>+</button>
         </form>
     </div>
     )
