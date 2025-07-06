2b1944502c8d02d94c0cd69c8f177846c9d24a85
marceloAst <marcelo.2007@alunos.utfpr.edu.br>
2025-07-06 18:08:11 -0300

    1.4

--- src/NewCategory.jsx
+++ src/NewCategory.jsx
@@ -1,4 +1,5 @@
 import "./PopUp.css"
+import "./NewCategory.css"
 
 export default function NewCategory({data, setData, setControl}){
     let categoria = ""
@@ -9,12 +10,14 @@ export default function NewCategory({data, setData, setControl}){
         setData(newData)
         setControl(false)
     }
-    return(<div className="PopUp novaCategoria">
-        <button onClick={()=>{setControl(false)}}><i className="bi bi-x"></i></button>
+    return(<div className="PopUp NovaCategoria novaCategoria">
+        <button className="x" onClick={()=>{setControl(false)}}><i className="bi bi-x"></i></button>
         <h1>Nova Categoria</h1>
         <form onSubmit={adicionarCategoria}>
-            <input type="text" name="category" id="category" onChange={(e)=>{categoria = e.target.value}} placeholder="Nome da categoria" required/>
-            <button>+</button>
+           <div className="Epaco">
+             <input className="InputNovacategoria" type="text" name="category" id="category" onChange={(e)=>{categoria = e.target.value}} placeholder="Nome da categoria" required/>
+            <button className="ButtonNovacategoria" >+</button>
+           </div>
         </form>
     </div>
     )
